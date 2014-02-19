19545a6c6ae24a0f2ba1e1f4f31c82ca905f206e
Hyosup Shim <flourscent@gmail.com>
2014-02-20 02:32:32 +0900

    initial implementation for sparql template generation

--- sparqlTemplateGenerator/src/main/java/kr/ac/postech/isoft/okbqa/sparql/SparqlTemplateGenerator.java
+++ sparqlTemplateGenerator/src/main/java/kr/ac/postech/isoft/okbqa/sparql/SparqlTemplateGenerator.java
@@ -1,11 +1,111 @@
 package kr.ac.postech.isoft.okbqa.sparql;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import com.clearnlp.dependency.DEPNode;
+import com.clearnlp.dependency.DEPTree;
+import com.clearnlp.reader.AbstractReader;
 
 public class SparqlTemplateGenerator {
-
+	public static final String DEF_LANG = AbstractReader.LANG_EN;
+	public static final String TEST_QUESTION = "In which country does the Nile start?";
+	
+	public static final String[] w5h1 = { "where", "when", "why", "which", "what", "how" };
+	
+	public static QuestionProcessor qp;
+	static {
+		try {
+			qp = new QuestionProcessor(QuestionProcessor.DEF_LANG, QuestionProcessor.DEF_MODELTYPE);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public static List<PseudoQueryCandidate> getCandidates() {
-		
 		return null;
+	}
+	
+	public static void getPatterns(String question) {
+		DEPTree parsed = qp.process(question);
+		DEPTree parsedClone = parsed.clone();
+		
+		Map<String, Double> lambda = new HashMap<String, Double>();
+		lambda.put("pos", 0.25);
+		lambda.put("dep", 0.25);
+		lambda.put("syn", 0.25);
+		lambda.put("srl", 0.25);
+		
+		Map<String, List> rules;
+		rules = new HashMap<String, List>();
+		
+		List<String> posPtns = new ArrayList<String>();
+		posPtns.add("NN");
+		posPtns.add("NNS");
+		posPtns.add("NNP");
+		posPtns.add("NNPS");
+		posPtns.add("VB");
+		posPtns.add("VBD");
+		posPtns.add("VBG");
+		posPtns.add("VBN");
+		posPtns.add("VBP");
+		posPtns.add("VBZ");
+		
+		List<String> depPtns = new ArrayList<String>();
+		depPtns.add("");
+		
+		List<String> synPtns = new ArrayList<String>();
+		synPtns.add("");
+		
+		List<String> srlPtns = new ArrayList<String>();
+		srlPtns.add("");
+		
+		//List<String> collectedNPs = new ArrayList<String>();
+		List<DEPNode> collectedVNodes = new ArrayList<DEPNode>();
+		List<DEPNode> collectedNNodes = new ArrayList<DEPNode>();
+		rules.put("pos", posPtns);
+		rules.put("dep", depPtns);
+		rules.put("syn", synPtns);
+		rules.put("srl", srlPtns);
+		
+		// rule1 : collect nouns that its head is not a noun.
+		// rule2 : collect verbs
+		// root 부터 순회를 하는데,
+		
+		// on pos level.
+		Iterator<DEPNode> it = parsed.iterator();
+		while (it.hasNext()) {
+			DEPNode currNode = it.next();
+			if (posPtns.contains(currNode.pos)) {
+				 if (currNode.pos.startsWith("N")) {
+					DEPNode head = currNode.getHead();
+					if (!head.pos.startsWith("N")) {
+						collectedNNodes.add(currNode);
+					}
+				 } else if (currNode.pos.startsWith("V")) {
+					List<DEPNode> roots = parsed.getRoots();
+					//if (roots.contains(currNode)) {
+					collectedVNodes.add(currNode);
+					//}
+				 }
+			}
+		}
+		
+		// on dependency level.
+		it = parsed.iterator();
+		DEPNode root = parsed.getRoots().get(0);
+		while (it.hasNext()) {
+			DEPNode currNode = it.next();
+			
+			//System.out.println(currNode.getSLabel(root));
+			for (DEPNode currRoot : collectedVNodes) {
+				System.out.println(currNode.getSLabel(currRoot));
+			}
+		}
 	}
 }
--- sparqlTemplateGenerator/src/main/java/kr/ac/postech/isoft/okbqa/sparql/Test.java
+++ sparqlTemplateGenerator/src/main/java/kr/ac/postech/isoft/okbqa/sparql/Test.java
@@ -1,82 +1,31 @@
 package kr.ac.postech.isoft.okbqa.sparql;
 
-import java.io.BufferedReader;
-import java.io.PrintStream;
-import java.util.List;
+import java.io.IOException;
 
-import com.clearnlp.component.AbstractComponent;
-import com.clearnlp.dependency.DEPTree;
-import com.clearnlp.nlp.NLPGetter;
-import com.clearnlp.nlp.NLPMode;
-import com.clearnlp.reader.AbstractReader;
-import com.clearnlp.segmentation.AbstractSegmenter;
-import com.clearnlp.tokenization.AbstractTokenizer;
-
-/**
- * @since 1.1.0
- * @author Jinho D. Choi ({@code jdchoi77@gmail.com})
- */
 public class Test
 {
-	final String language = AbstractReader.LANG_EN;
-
-	public Test(String modelType, String inputFile, String outputFile) throws Exception
-	{
-		AbstractTokenizer tokenizer  = NLPGetter.getTokenizer(language);
-		AbstractComponent tagger     = NLPGetter.getComponent(modelType, language, NLPMode.MODE_POS);
-		AbstractComponent parser     = NLPGetter.getComponent(modelType, language, NLPMode.MODE_DEP);
-		AbstractComponent identifier = NLPGetter.getComponent(modelType, language, NLPMode.MODE_PRED);
-		AbstractComponent classifier = NLPGetter.getComponent(modelType, language, NLPMode.MODE_ROLE);
-		AbstractComponent labeler    = NLPGetter.getComponent(modelType, language, NLPMode.MODE_SRL);
-
-		AbstractComponent[] components = {tagger, parser, identifier, classifier, labeler};
-
-		String sentence = "Who is Horatio Nelson.";
-		process(tokenizer, components, sentence);
-		//process(tokenizer, components, UTInput.createBufferedFileReader(inputFile), UTOutput.createPrintBufferedFileStream(outputFile));
-	}
-
-	public void process(AbstractTokenizer tokenizer, AbstractComponent[] components, String sentence)
-	{
-		DEPTree tree = NLPGetter.toDEPTree(tokenizer.getTokens(sentence));
-
-		for (AbstractComponent component : components)
-			component.process(tree);
-
-		System.out.println(tree.toStringSRL()+"\n");
-	}
-
-	public void process(AbstractTokenizer tokenizer, AbstractComponent[] components, BufferedReader reader, PrintStream fout)
-	{
-		AbstractSegmenter segmenter = NLPGetter.getSegmenter(language, tokenizer);
-		DEPTree tree;
-
-		for (List<String> tokens : segmenter.getSentences(reader))
-		{
-			tree = NLPGetter.toDEPTree(tokens);
-
-			for (AbstractComponent component : components)
-				component.process(tree);
-
-			fout.println(tree.toStringSRL()+"\n");
-		}
-
-		fout.close();
-	}
-
-	public static void main(String[] args)
-	{
-		//String modelType  = args[0];	// "general-en" or "medical-en"
-		String modelType = "general-en";
-		//String inputFile  = args[1];
-		String inputFile = "iphone5.txt";
-		//String outputFile = args[2];
-		String outputFile = "iphone5_output.txt";
-
-		try
-		{
-			new Test(modelType, inputFile, outputFile);
-		}
-		catch (Exception e) {e.printStackTrace();}
+	public static void main(String[] args) throws IOException {
+//		QuestionProcessor qp = new QuestionProcessor(QuestionProcessor.DEF_LANG, QuestionProcessor.DEF_MODELTYPE);
+//		String question = "Who is the brother of Marc?";
+		String q2 = "How many students does the free university in Amsterdam have?";
+		String q3 = "In which country does the Nile start?";
+//		
+//		File questionFile = new File("qald_questions.txt");
+//		
+//		BufferedReader br = new BufferedReader(new FileReader(questionFile));
+//		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("output.txt")));
+//		
+//		for (String x = br.readLine(); x != null ; x = br.readLine()) {
+//			DEPTree parsed = qp.process(x);
+//			//System.out.println(parsed.toStringSRL()+"\n");
+//			bw.append(parsed.toStringSRL()+"\n\n");
+//		}
+		
+		SparqlTemplateGenerator.getPatterns(q2);
+		
+		//DEPTree parsed = qp.process(question);
+		//DEPTree parsed = qp.process(q2);
+		
+		//System.out.println(parsed.toStringSRL()+"\n");
 	}
 }
